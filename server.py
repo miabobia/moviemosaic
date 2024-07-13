bf05e8b8a746be400d6a9062ddf04de72e3a0146
Mia Abbott <38595117+mamamia96@users.noreply.github.com>
2024-07-12 21:31:09 -0600

    Update server.py

--- server.py
+++ server.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 def hello_world():
     print("new web request")
     return f"hello from disco!!! the datetime is {datetime.now()}"
+@app.route("/test")
+def test():
+    return f'THIS IS A TEST!!! IM ON THE INTERNET :3'
 
 
 if __name__ == "__main__":
