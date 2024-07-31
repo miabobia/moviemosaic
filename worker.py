ab193ff316c36a03f3690f051dcfdb9b72aa0609
Mia Abbott <38595117+mamamia96@users.noreply.github.com>
2024-07-31 14:01:48 -0600

    updating worker schema

--- worker.py
+++ worker.py
@@ -101,8 +101,6 @@ def main(db: sqlite3.Connection):
 if __name__ == '__main__':
     db = sqlite3.connect(DATABASE)
     cur = db.cursor()
-    cur.execute('DROP TABLE IF EXISTS TASKS')
-    cur.execute('DROP TABLE IF EXISTS RESULTS')
     cur.execute("CREATE TABLE IF NOT EXISTS TASKS(id, user, mode, progress_msg, status, error_msg)")
     cur.execute("CREATE TABLE IF NOT EXISTS RESULTS(id, result, created_on)")
     cur.close()
