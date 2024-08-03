153c81a31f41e625118174d5d6fdcf8d88347a6b
mamamia96 <matchu2007@gmail.com>
2024-08-02 20:20:14 -0600

    table cleaner

---
+++ refresh_tables.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+
+db = sqlite3.connect("/sqlitedata/database.sqlite3")
+
+db.exec("DROP TABLE IF EXISTS TASKS")
+db.exec("DROP TABLE IF EXISTS RESULTS")
+db.commit()
+cur = db.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS TASKS(id, user, mode, progress_msg, status, error_msg)")
+cur.execute("CREATE TABLE IF NOT EXISTS RESULTS(id, result, created_on)")
+db.commit()
+db.close()
