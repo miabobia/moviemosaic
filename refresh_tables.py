c8e7aaafb79e8b399816a10fc74b46f60d4f156d
mamamia96 <matchu2007@gmail.com>
2024-08-02 20:24:55 -0600

    fix syntax err

--- refresh_tables.py
+++ refresh_tables.py
@@ -3,8 +3,8 @@ import sqlite3
 
 db = sqlite3.connect("/sqlitedata/database.sqlite3")
 
-db.exec("DROP TABLE IF EXISTS TASKS")
-db.exec("DROP TABLE IF EXISTS RESULTS")
+db.execute("DROP TABLE IF EXISTS TASKS")
+db.execute("DROP TABLE IF EXISTS RESULTS")
 db.commit()
 cur = db.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS TASKS(id, user, mode, progress_msg, status, error_msg)")
