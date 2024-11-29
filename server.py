18e8ea8386d4c5a4239ba5b4374a71f858c328a2
mamamia96 <matchu2007@gmail.com>
2024-11-29 14:38:38 -0500

    pathing debugging woop woop

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ from io import BytesIO
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.config["UPLOAD_FOLDER"] = "/results"
+app.config["UPLOAD_FOLDER"] = "results"
 app.secret_key = secrets.token_urlsafe(16)
 Session(app)
 
